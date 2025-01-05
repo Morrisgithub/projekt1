4fb1cb475172d74c43254072e8b4a68de124a9bc
Robert Vu <vurobert.rv@gmail.com>
2025-01-05 17:15:52 +0100

    Start på app

---
+++ MiniräknareMorrisApp/MiniräknareMorris/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MiniräknareMorris
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator add = new Calculator();
+            add.Sum(1,2);
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
+public class Calculator
+{
+    public void Sum(int x, int y)
+    {
+       
+        Console.WriteLine($"{x} + {y} = {x + y}" );
+            
+    }
+}
